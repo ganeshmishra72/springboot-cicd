3eceb42e8d888ee69ce046b9432c0f82622ee8a4
Ganesh <ganesh@dev.in>
2026-07-24 20:59:54 +0530

    deploy cicd

---
+++ src/main/java/com/rdmishra/springbootcicd/Controller/Welcome.java
@@ -0,0 +1,16 @@
+package com.rdmishra.springbootcicd.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/welcome")
+public class Welcome {
+
+    @GetMapping
+    public ResponseEntity<String> welcome() {
+        return ResponseEntity.ok("Welcome to spring boot CI/CD");
+    }
+}
